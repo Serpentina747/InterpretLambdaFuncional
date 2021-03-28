b5b31dabc424a2763174286915490de0a97156fc
Serpentina747 <sergi.petrache@gmail.com>
2021-03-28 18:12:46 +0200

    Funcions fetes per retornar variables lliures i lligades del lambda terme

--- practica.hs
+++ practica.hs
@@ -4,3 +4,10 @@ data LT = Variable Var | Abstr Var LT | Appli LT LT deriving (Eq, Show)
 
 freeVars :: LT -> [Var]
 freeVars (Variable x) = [x]
+freeVars (Abstr x y) = filter (/= x) (freeVars y)
+freeVars (Appli x y) = freeVars x ++ freeVars y
+
+boundVars :: LT -> [Var]
+boundVars (Variable x) = [x]
+boundVars (Abstr x y) = filter (== x) (freeVars y)
+boundVars (Appli x y) = freeVars x ++ freeVars y
