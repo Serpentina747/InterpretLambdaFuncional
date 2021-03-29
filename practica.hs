6640aa801484a4095ebc7899ed6bdcf3fa52d1b5
Serpentina747 <sergi.petrache@gmail.com>
2021-03-29 15:24:08 +0200

    Subst fuction done

--- practica.hs
+++ practica.hs
@@ -12,9 +12,15 @@ boundVars (Variable x) = [x]
 boundVars (Abstr x y) = filter (== x) (freeVars y)
 boundVars (Appli x y) = freeVars x ++ freeVars y
 
-hies :: Var -> [Var] -> Bool
-hies _ [] = False
-hies y (x:xs) = x == y || hies y xs
+inlist :: Var -> [Var] -> Bool
+inlist _ [] = False
+inlist y (x:xs) = x == y || inlist y xs
+
+
+getAlfaValue :: [Var] -> LT -> LT -> Var
+intlist [] _ _ = error "There's no alfa value for alfa conversion"
+getAlfaValue  (x:xs) y z = if not(inlist x (freeVars y)) && not(inlist x (freeVars z)) then x else getAlfaValue xs y z
+
 
 subst :: LT -> Var -> LT -> LT
 subst (Variable x) y z
@@ -23,7 +29,6 @@ subst (Variable x) y z
 subst (Appli term_a term_b) subst_value new_value = Appli (subst term_a subst_value new_value) (subst term_b subst_value new_value)
 subst (Abstr abstr_value abstr_term) subst_value new_value
  | abstr_value == subst_value = Abstr abstr_value abstr_term
- | abstr_value /= subst_value && not(hies abstr_value (freeVars new_value)) = Abstr abstr_value (subst abstr_term subst_value new_value)
- -- | abstr_value /= subst_value && hies abstr_value (freeVars new_value) = Abstr (alfa_value abstr_term new_value) (subst abstr_term abstr_value (Variable alfa_value))
-    -- where
-        -- alfa_value  
+ | abstr_value /= subst_value && not(inlist abstr_value (freeVars new_value)) = Abstr abstr_value (subst abstr_term subst_value new_value)
+ | abstr_value /= subst_value && inlist abstr_value (freeVars new_value) = Abstr (getAlfaValue ['a'..'z'] abstr_term new_value) 
+                                        (subst (subst abstr_term abstr_value (Variable (getAlfaValue ['a'..'z'] abstr_term new_value))) subst_value new_value)
